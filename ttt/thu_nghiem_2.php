cae9f8cd8dae45092ea4ae4b82a415a3952945d1
unknown <tomhumchin2@gmail.com.vn>
2020-11-18 02:45:12 +0700

    thay doi ti a

--- ttt/thu_nghiem_2.php
+++ ttt/thu_nghiem_2.php
@@ -16,25 +16,21 @@
    $KETNOI = mysqli_query($conn,$sql);
    $dd = array();
    $i = 0;
-  while($row = mysqli_fetch_array($KETNOI))
+  //while($row = mysqli_fetch_array($KETNOI))
   {
-    //$row = mysqli_fetch_array($KETNOI);
+    $row = mysqli_fetch_array($KETNOI);
     array_push($dd,array(
       "label"=> "Tiền shopping",
       "y"=> $row[0]),
       array(
         "label"=> "Tiền Mua sắm hằng ngày",
-        "y"=> $row[1]),
+        "y"=> $row[1])
     );
    // return;
     $i=$i+1;
     
   }
-  $dataPoints7 = array(
-    array("label"=> "Tiền shopping", "y"=> 50),
-    array("label"=> "Tiền Mua sắm hằng ngày", "y"=> 9),
-    array("label"=> "Tiền đầu tư", "y"=> 90)
-  );
+ 
   //print_r($dd);
  echo "<pre>";
  print_r($dd);
@@ -48,44 +44,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-<body>
-  <script>
-    window.onload = function ()
-     {
+<script>
+window.onload = function() {
  
- var chart1 = new CanvasJS.Chart("chartContainer", {
-   title: {
-     text: "Spending of Money Based on Household Composition"
-   },
-   theme: "light1",
-   animationEnabled: true,
-   toolTip:{
-     shared: true,
-     reversed: true
-   },
-   
-   data: 
-   [
-     {
-       type: "stackedColumn100",
-       name: "Tháng 1",
-       showInLegend: true,
-       yValueFormatString: "$#,##0 K",
-       dataPoints: <?php echo json_encode($dd, JSON_NUMERIC_CHECK); ?>
-     },
-     {
-       type: "stackedColumn100",
-       name: "Tháng 2",
-       showInLegend: true,
-       yValueFormatString: "$#,##0 K",
-       dataPoints: <?php echo json_encode($dataPoints7, JSON_NUMERIC_CHECK); ?>
-     }
-   ]
- });
- chart1.render();
-    }
-  </script>
-  <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2",
+	animationEnabled: true,
+	title: {
+		text: "World Energy Consumption by Sector - 2012"
+	},
+	data: [{
+		type: "pie",
+		indexLabel: "{y}",
+		yValueFormatString: "#,##0.00\"%\"",
+		indexLabelPlacement: "inside",
+		indexLabelFontColor: "#36454F",
+		indexLabelFontSize: 18,
+		indexLabelFontWeight: "bolder",
+		showInLegend: true,
+		legendText: "{label}",
+		dataPoints: <?php echo json_encode($dd, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>
